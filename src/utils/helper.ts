f9399fcc423d7211780fd45a6ec615d619451342
jenishgojariya <115468408+jenishgojariya@users.noreply.github.com>
2025-09-28 16:46:01 +0530

    let const use deployment error solved

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -184,9 +184,9 @@ export function generateName() {
 
 export function getRandomMessage(length: number) {
   let result = "";
-  let characters =
+  const characters =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let charactersLength = characters.length;
+  const charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
